0a27d92d1ec262be9745a894aba5661fcb18faa8
parthk02 <parthsunilkashyap@gmail.com>
2025-06-08 19:28:50 +0530

    updated vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
  
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase the limit to hide warning
+  },
   plugins: [react()],
   resolve: {
     alias: {
